a0d5bc55b39a97f9a607a6336c11f80019ba1041
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-01-21 21:29:26 +0200

    Remove staticrypt configuration and add bundle obfuscator plugin to Vite

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import vitePluginBundleObfuscator from 'vite-plugin-bundle-obfuscator'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), vitePluginBundleObfuscator()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
